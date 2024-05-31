86cc712478142794ee688b24133bf20e117ee108
fisunoff <fisunoff@yandex.ru>
2024-05-31 20:37:43 +0300

    ручка

--- backend/updater/views.py
+++ backend/updater/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+from utils.getfile import update_data
+
+__all__ = [
+    'UpdaterView',
+]
+
+
+class UpdaterView(APIView):
+    def post(self, request):
+        update_data()
+        return Response(status=status.HTTP_201_CREATED)
